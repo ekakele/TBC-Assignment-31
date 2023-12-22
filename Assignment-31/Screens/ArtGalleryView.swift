51e6c9c17e2b7cc16d5be381e2c91ee8c5fc5544
ekakele <ekaakelenjeridze@gmail.com>
2023-12-22 20:21:17 +0400

    organize folder strucutre

---
+++ Assignment-31/Screens/ArtGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtGalleryView.swift
+//  Assignment-31
+//
+//  Created by Eka Kelenjeridze on 22.12.23.
+//
+
+import SwiftUI
+
+struct ArtGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtGalleryView()
+}
