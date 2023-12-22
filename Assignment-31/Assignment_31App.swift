a01c8353b2fa60123a906771c3090accc5d76d86
ekakele <ekaakelenjeridze@gmail.com>
2023-12-22 20:18:42 +0400

    Initial commit

---
+++ Assignment-31/Assignment_31App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_31App.swift
+//  Assignment-31
+//
+//  Created by Eka Kelenjeridze on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_31App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
